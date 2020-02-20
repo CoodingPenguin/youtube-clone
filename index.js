967e93749802a60611851e2bce7d2a8c18574b5e
CoodingPenguin <unodostre@naver.com>
2020-02-21 03:09:38 +0900

    Add middlewares

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`Listening on: https://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hello from Groot!");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
