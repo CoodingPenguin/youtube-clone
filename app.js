8de54e1bbbe0624196dedde38c32f20c632f5221
CoodingPenguin <unodostre@naver.com>
2020-02-22 03:54:50 +0900

    Separate into routes and controllers

--- app.js
+++ app.js
@@ -3,26 +3,23 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
 
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: https://localhost:${PORT}`);
-
-const handleHome = (req, res) => res.send("Hello from Groot!");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
-
+// middlewares
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
+// routers
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
-app.get("/profile", handleProfile);
-
-app.listen(PORT, handleListening);
+export default app;
