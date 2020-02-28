43b24282af94ac5a243d42235ac4fe2e1801e5cb
CoodingPenguin <unodostre@naver.com>
2020-02-28 20:54:00 +0900

    Complete login and join logic. (authentication, verifying etc.)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,39 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
-export const login = (req, res) => res.render("login", { pageTitle: "Log In" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    // To Do: Register User
+    // To Do: Log user in
+    res.redirect(routes.home);
+  }
+  res.render("join", { pageTitle: "Join" });
+};
+
+export const getlogin = (req, res) =>
+  res.render("login", { pageTitle: "Log In" });
+
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Log Out" });
+
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "User Detail" });
+
 export const editProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "Edit Profile" });
+
 export const changePassword = (req, res) =>
   res.render("changePassword", { pageTitle: "Change Password" });
