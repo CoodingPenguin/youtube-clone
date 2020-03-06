242f12aefb78c08591ba8cad72f87e0493ac7fa6
CoodingPenguin <unodostre@naver.com>
2020-03-06 22:33:49 +0900

    Modify gitignore

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -14,10 +14,18 @@ export const home = async (req, res) => {
 };
 
 // search
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: { $regex: searchingBy, $options: "i" }
+    });
+  } catch (error) {
+    console.log(error);
+  }
   res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
