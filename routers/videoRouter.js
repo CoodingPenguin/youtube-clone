2d8d95bc810023963f99fa57cb0edfc8a3ad213d
CoodingPenguin <unodostre@naver.com>
2020-03-06 21:22:48 +0900

    Bug Fix: POST가 안되면 내가 실수로 post가 아니라 get을 쓴 거

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -4,17 +4,26 @@ import {
   getUpload,
   postUpload,
   videoDetail,
-  editVideo,
-  deleteVideo
+  deleteVideo,
+  getEditVideo,
+  postEditVideo
 } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
+// upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+
+// video details
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+
+// edit video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// delete video
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
