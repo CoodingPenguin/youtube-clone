c2f724ece3a569385f134aee3e2c38da2aa151b1
CoodingPenguin <unodostre@naver.com>
2020-03-11 14:21:18 +0900

    Make User Schema

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,11 @@
 import routes from "../routes";
+import User from "../models/User";
 
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
   const {
     body: { name, email, password, password2 }
   } = req;
@@ -12,8 +13,15 @@ export const postJoin = (req, res) => {
     res.status(400);
     res.render("join", { pageTitle: "Join" });
   } else {
-    // To Do: Register User
-    // To Do: Log user in
+    try {
+      const user = await User({
+        name,
+        email
+      });
+      await User.register(user, password);
+    } catch (error) {
+      console.log(error);
+    }
     res.redirect(routes.home);
   }
   res.render("join", { pageTitle: "Join" });
