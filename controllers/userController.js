cf7a862bb7e6785e6c3c83cf45b4ed9a5ae062d9
CoodingPenguin <unodostre@naver.com>
2020-03-17 01:34:07 +0900

    Make finding other user's profile and if error redirect to home

--- controllers/userController.js
+++ controllers/userController.js
@@ -75,8 +75,17 @@ export const getMe = (req, res) => {
   res.render("userDetail", { pageTitle: "User Detail", user: req.user });
 };
 
-export const userDetail = (req, res) =>
-  res.render("userDetail", { pageTitle: "User Detail" });
+export const userDetail = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const user = await User.findById(id);
+    res.render("userDetail", { pageTitle: "User Detail", user });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
 
 export const editProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "Edit Profile" });
