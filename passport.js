d4c55b7ed27b920bc1fc7d55d21d1347072acccb
CoodingPenguin <unodostre@naver.com>
2020-03-17 23:28:30 +0900

    Finish fxxking facebook login

--- passport.js
+++ passport.js
@@ -28,7 +28,9 @@ passport.use(
     {
       clientID: process.env.FB_ID,
       clientSecret: process.env.FB_SECRET,
-      callbackURL: `http://localhost:4000${routes.facebookCallback}`
+      callbackURL: `https://localhost:4000${routes.facebookCallback}`,
+      profileFields: ["id", "displayName", "photos", "email"],
+      scope: ["public_profile", "email"]
     },
     facebookLoginCallback
   )
